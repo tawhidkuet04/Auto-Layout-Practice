3e5a4126f628af499280229f4e44200884ace91f
Tawhid Joarder <tawhid@Sks-MacBook-Pro.local>
2019-04-07 13:12:43 +0600

    status bar hide

--- autoLayout/AppDelegate.m
+++ autoLayout/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [application setStatusBarHidden:YES];
     // Override point for customization after application launch.
     return YES;
 }
